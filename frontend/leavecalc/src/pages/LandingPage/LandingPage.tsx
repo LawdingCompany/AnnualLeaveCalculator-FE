390a3a7a8a9141e71c16d9f4d2d405511eca27f6
김재윤 <jaeyun1723@naver.com>
2025-05-08 23:55:18 +0900

    feat : 연차계산기 ui 추가

--- frontend/leavecalc/src/pages/LandingPage/LandingPage.tsx
+++ frontend/leavecalc/src/pages/LandingPage/LandingPage.tsx
@@ -4,7 +4,7 @@ import styles from './LandingPage.module.scss';
 export default function LandingPage() {
   return (
     <>
-      <div className={styles.wrapper}>
+      <div className={`${styles.wrapper} min-h-screen flex justify-center items-center p-5`}>
         <AnnualLeaveCalculatorCard />
       </div>
     </>
