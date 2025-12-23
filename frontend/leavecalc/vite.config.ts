b902b47fb165f963b8e0fe7681eaec1b8c081fff
김재윤 <jaeyun1723@naver.com>
2025-12-24 01:38:27 +0900

    fix : vite proxy

--- frontend/leavecalc/vite.config.ts
+++ frontend/leavecalc/vite.config.ts
@@ -9,9 +9,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/v1': {
+      '/api': {
         target: 'https://api.lawding.net',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
         secure: true,
       },
     },
