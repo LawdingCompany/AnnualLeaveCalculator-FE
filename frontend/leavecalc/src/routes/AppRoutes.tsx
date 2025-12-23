0f630d3b7406c7c5c88bfc88ac703e8a3b91bd58
김재윤 <jaeyun1723@naver.com>
2025-12-23 21:49:43 +0900

    fix : api

--- frontend/leavecalc/src/routes/AppRoutes.tsx
+++ frontend/leavecalc/src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import LandingPage from '@pages/LandingPage/LandingPage';
 import StatsPage from '@pages/AdminPage/StatsPage/StatsPage';
 import FeedbackPage from '@pages/AdminPage/FeedbackPage/feedbackPage';
 import NotFoundPage from '@pages/ErrorPage/ErrorPage';
+import DictionariesPage from '@pages/AdminPage/DictionaryPage/DictionaryPage';
 
 export default function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/admin/stats" element={<StatsPage />} />
         <Route path="/admin/feedback" element={<FeedbackPage />} />
+        <Route path="/admin/dictionaries" element={<DictionariesPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
